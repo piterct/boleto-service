c07ac801eb82636ae1c5fca85ca8430d147c07f1
Michael Peter <michael_piterct@hotmail.com>
2021-08-14 20:16:45 -0300

    BoletoBancario

---
+++ Boleto.Service/Boleto.Service.Domain/Entity/BoletoBancario.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Boleto.Service.Domain.Entity
+{
+    public class BoletoBancario
+    {
+        public BoletoBancario(string linhaDigitavel)
+        {
+            LinhaDigitavel = linhaDigitavel;
+        }
+
+        public string LinhaDigitavel { get; private set; }
+        public string CodigoBarras { get; set; }
+
+        public string CalculaCodigoBarras()
+        {
+            this.CodigoBarras = Regex.Match(this.LinhaDigitavel, @"\d+").Value;
+
+            return CodigoBarras;
+        }
+    }
+}
--- Boleto.Service/Boleto.Service.Domain/Handlers/BoletoHandler.cs
+++ Boleto.Service/Boleto.Service.Domain/Handlers/BoletoHandler.cs
@@ -1,5 +1,6 @@
 ﻿using Boleto.Service.Domain.Commands.Input;
 using Boleto.Service.Domain.Commands.Result;
+using Boleto.Service.Domain.Entity;
 using Microsoft.AspNetCore.Http;
 using System.Threading.Tasks;
 
@@ -12,6 +13,10 @@ namespace Boleto.Service.Domain.Handlers
             command.Validate();
             if (command.Invalid)
                 return new ValidaLinhaDigitavelBoletoCommandResult(false, "Incorrect  data!", null, StatusCodes.Status400BadRequest, command.Notifications);
+
+            var boleto = new BoletoBancario(command.LinhaDigitavel);
+
+           var codigoBarras = boleto.CalculaCodigoBarras();
 
             return new ValidaLinhaDigitavelBoletoCommandResult(true, "Success!", null, StatusCodes.Status200OK, command.Notifications);
         }
