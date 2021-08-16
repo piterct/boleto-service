d36c5bc9ac5449d9deec0f27275ea5d1c4dcc1a5
Michael Peter <michael_piterct@hotmail.com>
2021-08-16 00:26:24 -0300

    Testes Command

--- Boleto.Service/Boleto.Service.Domain/Commands/Input/CalculaCodigoBarrasBoletoCommandInput.cs
+++ Boleto.Service/Boleto.Service.Domain/Commands/Input/CalculaCodigoBarrasBoletoCommandInput.cs
@@ -7,6 +7,17 @@ namespace Boleto.Service.Domain.Commands.Input
 {
     public class CalculaCodigoBarrasBoletoCommandInput : Notifiable
     {
+        public CalculaCodigoBarrasBoletoCommandInput()
+        {
+
+        }
+        public CalculaCodigoBarrasBoletoCommandInput(string linhaDigitavel, decimal valor, DateTime dataVencimento)
+        {
+            LinhaDigitavel = linhaDigitavel;
+            Valor = valor;
+            DataVencimento = dataVencimento;
+        }
+
         public string LinhaDigitavel { get; set; }
         public decimal Valor { get; set; }
         public DateTime DataVencimento { get; set; }
@@ -17,8 +28,8 @@ namespace Boleto.Service.Domain.Commands.Input
            new Contract()
                .Requires()
                .IsNotNullOrEmpty(LinhaDigitavel, "LinhaDigitavel", "LinhaDigitavel obrigatório!")
-                .IsGreaterThan(LinhaDigitavel.Length, 36, "LinhaDigitavel", "Tamanho mínimo 36 caracteres!")
-                .IsLowerThan(Regex.Match(this.LinhaDigitavel, @"\d+").Value.Length, 48, "LinhaDigitavel", "Tamanho máximo 47 caracteres!")
+                .IsGreaterThan(LinhaDigitavel == null ? 0 : LinhaDigitavel.Length, 36, "LinhaDigitavel", "Tamanho mínimo 36 caracteres!")
+                .IsLowerThan(LinhaDigitavel == null ? 0 : Regex.Match(this.LinhaDigitavel, @"\d+").Value.Length, 48, "LinhaDigitavel", "Tamanho máximo 47 caracteres!")
                 );
         }
     }
---
+++ Boleto.Service/Boleto.Service.Tests/Commands/CalculaCodigoBarrasBoletoCommandTests.cs
@@ -0,0 +1,34 @@
+﻿using Boleto.Service.Domain.Commands.Input;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Boleto.Service.Tests.Commands
+{
+    [TestClass]
+    public class CalculaCodigoBarrasBoletoCommandTests
+    {
+        [TestMethod]
+        public void Command_CalculaCodigoBarras_LinhaDigitavel_Legth_Menor_36_Invalido()
+        {
+            var command = new CalculaCodigoBarrasBoletoCommandInput("121545421", 20M, DateTime.Now);
+            command.Validate();
+            Assert.AreEqual(command.Valid, false, "Command de calculo inválido!");
+        }
+
+        [TestMethod]
+        public void Command_CalculaCodigoBarras_LinhaDigitavel_Valor_Nulo_Invalido()
+        {
+            var command = new CalculaCodigoBarrasBoletoCommandInput(null, 20M, DateTime.Now);
+            command.Validate();
+            Assert.AreEqual(command.Valid, false, "Command de calculo inválido!");
+        }
+
+        [TestMethod]
+        public void Command_CalculaCodigoBarras_LinhaDigitavel_Valida()
+        {
+            var command = new CalculaCodigoBarrasBoletoCommandInput("03399699255870000180185108001018874650000010000", 20M, DateTime.Now);
+            command.Validate();
+            Assert.AreEqual(command.Valid, true, "Command de calculo válido!");
+        }
+    }
+}
