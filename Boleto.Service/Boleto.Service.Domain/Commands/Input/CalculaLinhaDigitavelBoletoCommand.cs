184a3fef90d76c809785e9f19c7a1ac22b792a17
Michael Peter <michael_piterct@hotmail.com>
2021-08-21 23:37:57 -0300

    Calcula Linha Digitavel

---
+++ Boleto.Service/Boleto.Service.Domain/Commands/Input/CalculaLinhaDigitavelBoletoCommand.cs
@@ -0,0 +1,25 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using System;
+using System.Text.RegularExpressions;
+
+namespace Boleto.Service.Domain.Commands.Input
+{
+    public class CalculaLinhaDigitavelBoletoCommand : Notifiable
+    {
+        public string CodigoBarras { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime DataVencimento { get; set; }
+
+        public void Validate()
+        {
+            AddNotifications(
+           new Contract()
+               .Requires()
+               .IsNotNullOrEmpty(CodigoBarras, "CodigoBarras", "CodigoBarras obrigatório!")
+                .IsGreaterThan(CodigoBarras == null ? 0 : CodigoBarras.Length, 36, "CodigoBarras", "Tamanho mínimo 36 caracteres!")
+                .IsLowerThan(CodigoBarras == null ? 0 : Regex.Match(this.CodigoBarras, @"\d+").Value.Length, 48, "CodigoBarras", "Tamanho máximo 47 caracteres!")
+                );
+        }
+    }
+}
