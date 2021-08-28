7b8e58dd92a3e3289658eba95308dbdca782ac0d
Michael Peter <michael_piterct@hotmail.com>
2021-08-28 15:46:43 -0300

    valid maximum and minimum length  command

--- Boleto.Service/Boleto.Service.Domain/Commands/Input/CalculaLinhaDigitavelBoletoCommand.cs
+++ Boleto.Service/Boleto.Service.Domain/Commands/Input/CalculaLinhaDigitavelBoletoCommand.cs
@@ -8,8 +8,6 @@ namespace Boleto.Service.Domain.Commands.Input
     public class CalculaLinhaDigitavelBoletoCommand : Notifiable
     {
         public string CodigoBarras { get; set; }
-        public decimal Valor { get; set; }
-        public DateTime DataVencimento { get; set; }
 
         public void Validate()
         {
@@ -17,8 +15,8 @@ namespace Boleto.Service.Domain.Commands.Input
            new Contract()
                .Requires()
                .IsNotNullOrEmpty(CodigoBarras, "CodigoBarras", "CodigoBarras obrigatório!")
-                .IsGreaterThan(CodigoBarras == null ? 0 : CodigoBarras.Length, 36, "CodigoBarras", "Tamanho mínimo 36 caracteres!")
-                .IsLowerThan(CodigoBarras == null ? 0 : Regex.Match(this.CodigoBarras, @"\d+").Value.Length, 48, "CodigoBarras", "Tamanho máximo 47 caracteres!")
+                .IsLowerThan(CodigoBarras == null ? 0 : Regex.Match(this.CodigoBarras, @"\d+").Value.Length, 44, "CodigoBarras", "Tamanho mínimo 44 caracteres!")
+                .IsGreaterThan(CodigoBarras == null ? 0 : Regex.Match(this.CodigoBarras, @"\d+").Value.Length, 44, "CodigoBarras", "Tamanho máximo 44 caracteres!")
                 );
         }
     }
