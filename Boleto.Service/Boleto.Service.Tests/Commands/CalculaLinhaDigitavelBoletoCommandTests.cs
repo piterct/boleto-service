5788e73654df33f04bbc65ca4c64d05e000f6165
Michael Peter <michael_piterct@hotmail.com>
2021-09-02 00:26:08 -0300

    CalculaLinhaDigitavelBoletoCommandTests

---
+++ Boleto.Service/Boleto.Service.Tests/Commands/CalculaLinhaDigitavelBoletoCommandTests.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Boleto.Service.Tests.Commands
+{
+    [TestClass]
+    public class CalculaLinhaDigitavelBoletoCommandTests
+    {
+    }
+}
